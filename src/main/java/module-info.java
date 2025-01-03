09e9747549c76c87c8012fd98cb39636c15ea93b
abdallah <abdallah.alexcs@gmail.com>
2025-01-03 23:41:53 +0200

    login+admin+register+selectedmovie

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.cinema_reservation_system {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.logging;
 
 
     opens com.example.cinema_reservation_system to javafx.fxml;
