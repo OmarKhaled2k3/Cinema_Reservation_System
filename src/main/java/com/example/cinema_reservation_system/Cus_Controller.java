eb48b7473443a5c336d9cbfb42558054684b683b
abdallah <abdallah.alexcs@gmail.com>
2025-01-05 18:24:50 +0200

    customer view controller

---
+++ src/main/java/com/example/cinema_reservation_system/Cus_Controller.java
@@ -0,0 +1,47 @@
+package com.example.cinema_reservation_system;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Cus_Controller {
+
+    @FXML
+    private Label user_label;
+
+    @FXML
+    private Button btn_newreserv;
+
+    @FXML
+    private Button btn_mngreserv;
+
+    @FXML
+    private Button btn_logout;
+
+    // Method to handle "New Reservation" button action
+    @FXML
+    private void handleNewReservation (ActionEvent event) throws Exception {
+       SceneController.launchScene("SelectMovie.fxml");
+    }
+
+    // Method to handle "Manage Reservations" button action
+    @FXML
+    private void handleManageReservations(ActionEvent event) throws Exception {
+        SceneController.launchScene("Customer_ManageReservationTab.fxml");
+    }
+
+    // Method to handle "Log Out" button action
+    @FXML
+    private void handleLogout(ActionEvent event) {
+        System.out.println("Log Out button clicked!");
+        // Add logic for logging out, such as navigating to the login screen
+    }
+
+    // You can initialize the controller here
+    @FXML
+    public void initialize() {
+
+        user_label.setText("Guest");
+    }
+}
